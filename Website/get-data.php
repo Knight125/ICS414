57d402949e6cc3aac4a15f643e80703f3b2c1e50
Knight125 <knight2@hawaii.edu>
2015-12-05 23:25:27 -1000

    Made it look nicer

--- Website/get-data.php
+++ Website/get-data.php
@@ -27,29 +27,36 @@ if ($result->num_rows > 0) {
 } else {
      echo "empty";
 }
-
+$myfile = fopen("testpage.html", "w") or die("Unable to open file!");
 $html =<<<EOT
           <!DOCTYPE html>
           <html lang="en">
           <head>
             <meta charset="UTF-8">
             <title>Weather</title>
+            <link rel="stylesheet" href="text.css">
 EOT;
 switch($id)
 {
      case 1: $html .=<<<EOT
              <link rel="stylesheet" href="style1.css">
               </head>
-              <body>
+              <body><center><h1>
 EOT;
-             $html .=$weather.$temp;
+$html .=$weather.$temp;
              $html .=<<<EOT
+
+              </h1><form action="get-data.php" method="post">
+                  <input type="text" placeholder="city name" name="city"><br>
+                  <input type="submit" value ="See Weather">
+                </form></center>
               <div class="container-sun">
               <svg class="svg-sun" version="1.1" viewBox="0 0 100 100" preserveAspectRatio="xMinYMin meet">
               <circle cx="50" cy="50" r="35" id="sun"></circle>
               </svg>
               </div>
 EOT;
+
                break;
      case 2: $html .=<<<EOT
      <link rel="stylesheet" href="style2.css">
@@ -75,9 +82,15 @@ EOT;
          <div class="x5">
              <div class="cloud"></div>
          </div>
-     </div>
+     </div><center><h1>
 EOT;
      $html .= $weather.$temp;
+          $html .= <<<EOT
+            </h1><form action="get-data.php" method="post">
+              <input type="text" placeholder="city name" name="city"><br>
+              <input type="submit" value ="See Weather">
+            </form></center>
+EOT;
                   break;
      case 3:
      case 4: $html .=<<<EOT
@@ -85,9 +98,18 @@ EOT;
               </head>
               <body>
               <div class="container">
-                <div id="cloud-intro">
+                <div id="cloud-intro"><center><h1>
 EOT;
-               $html.=$weather.$temp.'</div> </div> <img src="http://btstrategy.com/wp-new/wp-content/uploads/2013/10/cloud2_55171579                 .jpg">';
+               $html.=$weather.$temp;
+               $html .=<<<EOT
+
+                </h1><form action="get-data.php" method="post">
+                   <input type="text" placeholder="city name" name="city"><br>
+                   <input type="submit" value ="See Weather">
+                 </form></center>
+               </div> </div></div>
+
+EOT;
           break;
 
      case 9:
@@ -138,10 +160,16 @@ overflow:hidden;}
         <script src="showerrain
 EOT;
      if($id == 9)
-     {$html .= '.js"></script>';}
+     {$html .= '.js"></script><center><h1>';}
      else
-     {$html .= '2.js"></script>';}
+     {$html .= '2.js"></script><center><h1>';}
      $html .= $weather.$temp;
+     $html .= <<<EOT
+       </h1><form action="get-data.php" method="post">
+         <input type="text" placeholder="city name" name="city"><br>
+         <input type="submit" value ="See Weather">
+       </form></center>
+EOT;
      break;
 
      default:
@@ -154,17 +182,20 @@ EOT;
                </style>
            </head>
            <body>
-               <canvas id="can"></canvas>
+               <canvas id="can"></canvas><h1>
 EOT;
         $html .= $weather.$temp;
+
         break;
 
 }
 $html.=<<<EOT
-             </body>
+             </h1></body>
              </html>
-EOT;
 
-echo $html;
+EOT;
+fwrite($myfile, $html);
+fclose($myfile);
+header("Location: testpage.html");
 $conn->close();
 ?>
